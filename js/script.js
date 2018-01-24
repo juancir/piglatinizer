860c254daf33c96f0a726fddbf6650aa96200295
Juan Cirilo <juancirilo194@yahoo.com>
2018-01-24 23:17:03 +0000

    consonant clusters

--- js/script.js
+++ js/script.js
@@ -1,16 +1,11 @@
-// Pig Latin takes the first consonant (or consonant cluster) of an English 
-// word, moves it to the end of the word and suffixes an ay, or if a word
-// begins with a vowel you just add ay to the end. For example, pig becomes
-// igpay, banana becomes ananabay, and aadvark becomes aadvarkay.
-
 $(document).ready(function() {
 	$("button").click(function() {
 		var sentence = $("input").val();
 		var newSentence = sentenceToPigLatin(sentence);
 		$("#newSentence").html(newSentence);
 	});
-	
-	function sentenceToPigLatin(sentence){
+
+	function sentenceToPigLatin(sentence) {
 		var words = sentence.split(" ");
 		var answers = [];
 		for (var i = 0; i < words.length; i++) {
@@ -18,19 +13,37 @@ $(document).ready(function() {
 		}
 		return answers.join(" ");
 	}
+	function indexOfFirstVowel(word) {   // "strip" -> 3
+		for (var i=0; i<word.length;i++){
+			// is the character at this index a vowel?
+			var letter = word[i];
+			var letterIsVowel = isVowel(letter);
+			
+			// if yes,
+			if(letterIsVowel == true){
+				return i;
+			}
+		}
+	}
 	
-	function wordToPigLatin(word) {
+	function isVowel(letter) {
 		var vowels = ["a", "e", "i", "o", "u"];
+		return vowels.indexOf(letter) > -1;
+	}
+
+	function wordToPigLatin(word) {
 		var firstLetter = word.slice(0, 1);
-		var indexOf = vowels.indexOf(firstLetter);
-		var beginsWithVowel = indexOf > -1;
+		var beginsWithVowel = isVowel(firstLetter);
+
 		if (beginsWithVowel) {
 			return word + "yay";
-		} else {
-			return word.slice(1) + firstLetter + "ay";
+		}
+		else {
+			var i = indexOfFirstVowel(word);
+		    var consonantCluster = word.slice(0,i);
+			return word.slice(i) + consonantCluster + "ay";
 		}
 
 	}
-	
-});
 
+});
